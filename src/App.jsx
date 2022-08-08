b5a0c567769a82f35cd47d713ca2500c672f17bd
Tyler Maksymiw <tylermcodes@gmail.com>
2022-08-08 10:03:40 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import "./Assets/Styles/AppStyles/App.css"
+
+function App() {
+  return (
+    <main className="App">
+      <h1>Hello World!</h1>
+    </main>
+  );
+}
+
+export default App;
